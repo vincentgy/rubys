c6fdbd08e33e36bd0f1bf2b7a63c32791237b4e9
admin2 <admin2@admin2s-MacBook-Pro.local>
2015-11-01 22:45:51 +0800

    initial commit

---
+++ protected/config/config.php
@@ -0,0 +1,32 @@
+<?php return array (
+  'classes' => 
+  array (
+    0 => 'classes.*',
+    1 => 'extensions.*',
+    2 => 'classes.barcode.*',
+    3 => 'classes.payments.*',
+    4 => 'classes.delivery.*',
+    5 => 'classes.oauth.*',
+  ),
+  'theme' => 'default',
+  'urlFormat' => 'get',
+  'db' => 
+  array (
+    'type' => 'mysql',
+    'tablePre' => 'tiny_',
+    'host' => '127.0.0.1:3306',
+    'user' => 'root',
+    'password' => 'r00t',
+    'name' => 'rubytest',
+  ),
+  'route' => 
+  array (
+  ),
+  'extConfig' => 
+  array (
+    'controllerExtension' => 
+    array (
+      0 => 'ControllerExt',
+    ),
+  ),
+);?>
