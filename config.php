d2ded7b5d6d5dd513fbc5583a3024fb61a684d1e
root <root@ip-172-31-33-194.us-west-2.compute.internal>
2015-12-08 14:14:55 +0000

    add opencart shop

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://52.27.31.162/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://52.27.31.162/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/html/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencart/image/');
+define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'r00t');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
