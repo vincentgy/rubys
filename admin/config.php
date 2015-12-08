75633ae3b35ec5ba598de335dd1912871050ad14
root <root@ip-172-31-33-194.us-west-2.compute.internal>
2015-12-08 16:05:53 +0000

    db update

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://52.27.31.162/opencart/admin/');
-define('HTTP_CATALOG', 'http://52.27.31.162/opencart/');
+define('HTTP_SERVER', 'http://52.27.31.162/admin/');
+define('HTTP_CATALOG', 'http://52.27.31.162/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://52.27.31.162/opencart/admin/');
-define('HTTPS_CATALOG', 'http://52.27.31.162/opencart/');
+define('HTTPS_SERVER', 'http://52.27.31.162/admin/');
+define('HTTPS_CATALOG', 'http://52.27.31.162/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
-define('DIR_SYSTEM', '/var/www/html/opencart/system/');
-define('DIR_LANGUAGE', '/var/www/html/opencart/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/opencart/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
-define('DIR_IMAGE', '/var/www/html/opencart/image/');
-define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
-define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'r00t');
-define('DB_DATABASE', 'opencart');
+define('DB_DATABASE', 'starmall');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_PREFIX', 'star_');
