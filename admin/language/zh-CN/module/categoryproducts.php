a2d2616e1cf0e05f648ec479d2b116c43cc95238
admin2 <admin2@admin2s-MacBook-Pro.local>
2015-12-09 22:40:24 +1300

    add cn

---
+++ admin/language/zh-CN/module/categoryproducts.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'Category Products';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified Category Products module!';
+$_['text_edit']        = 'Edit Category Products Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_product']    = 'Category Name';
+$_['entry_limit']      = 'Limit';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+
+// Help
+$_['help_product']     = '(Autocomplete)';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Category Products module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
